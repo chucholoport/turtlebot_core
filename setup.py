536c4e8554750f278389fbce46dd68820811a6b9
jesus.loport <jesus.loport@outlook.com>
2026-03-31 11:32:09 -0600

    ROS 2 package code base part 2

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtlebot_core'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jesus.loport',
+    maintainer_email='jesus.loport@outlook.com',
+    description='TurtleBot Core Package',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
