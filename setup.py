b2c7fab555e476b8ec95b63bf6eaac39822105e7
jesus.loport <jesus.loport@outlook.com>
2026-04-01 11:47:44 -0600

    local & remote infrastructure for rplidar

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'turtlebot_core'
 
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (f'share/{package_name}/launch', glob('launch/*.launch.py')),
+        (f'share/{package_name}/rviz', glob('rviz/*.rviz')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
